f9c53b80a06527f5b50dd64d32efc4980b1fa3f3
gund <malkevich.alex@gmail.com>
2020-03-08 02:15:26 +0100

    test(build): compile TS to commonjs modules
    So spyOn is working properly

--- projects/gen-io-ts/src/lib/property.spec.ts
+++ projects/gen-io-ts/src/lib/property.spec.ts
@@ -3,19 +3,15 @@ import { Property } from './property';
 
 describe('@Property() property decorator', () => {
   it('should return function that calls `setPropertyType()` with target, prop and options', () => {
-    const setPropertyType = fixedSpyOn(metadata, 'setPropertyType');
-    const fn = Property('options' as any);
+    const setPropertyType = spyOn(metadata, 'setPropertyType');
+    const options = 'options' as any;
+
+    const fn = Property(options);
 
     expect(fn).toEqual(jasmine.any(Function));
 
     fn('target', 'prop');
 
-    expect(setPropertyType).toHaveBeenCalledWith('target', 'prop', 'options');
+    expect(setPropertyType).toHaveBeenCalledWith('target', 'prop', options);
   });
 });
-
-function fixedSpyOn<T>(target: T, prop: keyof T): jasmine.Spy {
-  const spy = jasmine.createSpy(`${prop}Spy`);
-  spyOnProperty(target, prop).and.returnValue(spy);
-  return spy;
-}
